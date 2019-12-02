bde912c76b22c6101398bcf58b4fb22f67571ddf
austinwerner <r.austin.werner@gmail.com>
2019-12-01 18:20:59 -0600

    Hook up buttons
    Create placeholder functions for button callbacks

--- app/src/main/java/com/example/portfolio/MainActivity.kt
+++ app/src/main/java/com/example/portfolio/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.portfolio
 
+import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log
+import android.view.View
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -11,6 +14,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun handleMusicButton(v: View) {
+
+    }
+
+    fun handleArtButton(v: View) {
+        Log.d("WERNER","art button clicked")
+        val intent = Intent(this, Art::class.java)
+        startActivity(intent)
+    }
+
+    fun handleDesignButton(v: View) {
+
+    }
+
+    fun handlePhotosButton(v: View) {
 
     }
 }
